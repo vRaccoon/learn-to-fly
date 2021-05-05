69afe51638e22174068cc3bad1a6612c4ffa6dcb
vRaccoon <65045837+vRaccoon@users.noreply.github.com>
2021-05-05 09:11:18 +0200

    Update index.php

--- docker/index.php
+++ docker/index.php
@@ -17,7 +17,7 @@
 </head>
 <body>
         <img id="logo" src="logo.png" />
-        <h1><?php echo "<Type Your Message Here> - 25-11-2020 ".($_ENV["NAME"]?$_ENV["NAME"]:"world")."!"; ?></h1>
+        <h1><?php echo " - Type Your Message Here - - 25-11-2020 ".($_ENV["NAME"]?$_ENV["NAME"]:"world")."!"; ?></h1>
         <?php if($_ENV["HOSTNAME"]) {?><h3>My hostname is <?php echo $_ENV["HOSTNAME"]; ?></h3><?php } ?>
         <?php
         $links = [];
